78a840791d691a7f5d11deec0f3a7e3f6ccfb758
RibozymeR <ribozyme@web.de>
2023-04-17 20:38:07 +0200

    New file for finding reducing automorphisms This should probably be implemented as one set of methods returning Tuple{Word, Automorphism}

--- src/whiteheadaut.jl
+++ src/whiteheadaut.jl
@@ -7,28 +7,42 @@ Pkg.add("DataStructures")
 using Combinatorics
 using DataStructures
 
-# macro to turn our wh_reduceN into minimizer functions wh_minimizeN
-macro wh_minimize(num)
-    fun_name = Symbol("wh_minimize$num")
-    red_name = Symbol("wh_reduce$num")
+# macro to turn our wh_reduceN_aut into minimizer functions wh_minimizeN_aut
+macro wh_minimize_aut(num)
+    fun_name = Symbol("wh_minimize$(num)_aut")
+    red_name = Symbol("wh_reduce$(num)_aut")
     quote
         function $(esc(fun_name))(w::Word, X::Alphabet)
-            while (v = $(esc(red_name))(w, X)) !== nothing
-                w = v
+            σ = Automorphism(X)
+            while (τ = $(esc(red_name))(w, X)) !== nothing
+                σ = τ * σ
+                w = τ * w
             end
-            return w
+            return σ
         end
     end
 end
 
-@wh_minimize 1
-@wh_minimize 2
-@wh_minimize 3
+@wh_minimize_aut 1
+@wh_minimize_aut 2
+@wh_minimize_aut 3
+
+# test whether automorphism reduced given word, if so returns
+macro test_aut(expr, w)
+    quote
+        σ = $(esc(expr))
+        # circular reduction automorphisms get rather long, so we only compute it when necessary
+        if length(wreduce_circ(σ * $(esc(w)))) < length($(esc(w)))
+            return reduction(σ * $(esc(w)), X) * σ
+        end
+    end
+end
 
 """
-Simply try out all Whitehead automorphisms until one reduces length of the given word
+Simply try out all Whitehead automorphisms until one reduces length of the given word  
+Returns `nothing` if word cannot be reduced
 """
-function wh_reduce1(w::Word, X::Alphabet)
+function wh_reduce1_aut(w::Word, X::Alphabet)
     if length(w) <= 1
         return nothing
     end
@@ -37,11 +51,7 @@ function wh_reduce1(w::Word, X::Alphabet)
         X2 = setdiff(X.letters, [a, -a])
         for A in Combinatorics.powerset(X2)
             # if A is empty, this does nothing, but that's only 1/2^|X| of the iterations
-            σ = whitehead(A, a, X)
-            v = wreduce_circ(σ*w)
-            if length(v) < length(w)
-                return v
-            end
+            @test_aut whitehead(A, a, X) w
         end
     end
 
@@ -51,7 +61,7 @@ end
 """
 First try out all Nielsen automorphism, then try out all Whitehead automorphisms
 """
-function wh_reduce2(w::Word, X::Alphabet)
+function wh_reduce2_aut(w::Word, X::Alphabet)
     if length(w) <= 1
         return nothing
     end
@@ -60,25 +70,20 @@ function wh_reduce2(w::Word, X::Alphabet)
         for y in X
             y == x && continue
             y == -x && continue
-
             # x => x·y
-            σ = nielsen(x, y, X)
-            v = wreduce_circ(σ*w)
-            if length(v) < length(w)
-                return v
-            end
+            @test_aut nielsen(x, y, X) w
         end
     end
     # Nielsen automorphism x => y·x will be covered by x⁻¹ => x⁻¹·y⁻¹
 
     # this will test about |X| / 2^|X| automorphisms too much... not a big problem for |X| >> 1
-    return wh_reduce1(w, X)
+    return wh_reduce1_aut(w, X)
 end
 
 """
 First try out all Nielsen automorphism in advantageous order, then try out all Whitehead automorphisms
 """
-function wh_reduce3(w::Word, X::Alphabet)
+function wh_reduce3_aut(w::Word, X::Alphabet)
     if length(w) <= 1
         return nothing
     end
@@ -95,15 +100,9 @@ function wh_reduce3(w::Word, X::Alphabet)
 
     for ((x, y), n) in sort(collect(subwords), by=x -> x[2])
         # subword x·y in w
-        #  -> map x => x·y⁻¹
-        #  -> map y => x⁻¹·y  <- but this is handled by the equivalently-ranked subword y⁻¹·x⁻¹
-        σ = nielsen(x, -y, X)
-        v = wreduce_circ(σ*w)
-        if length(v) < length(w)
-            return v
-        end
+        @test_aut nielsen(x, -y, X) w
     end
 
     # this will test about |X| / 2^|X| automorphisms too much... not a big problem for |X| >> 1
-    return wh_reduce1(w, X)
+    return wh_reduce1_aut(w, X)
 end
