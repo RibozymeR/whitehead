7526d64095f48fdab406c1b304c3707ed698c412
RibozymeR <ribozyme@web.de>
2023-04-17 20:36:49 +0200

    Implemented generating automorphisms for circular reduction

--- src/automorphisms.jl
+++ src/automorphisms.jl
@@ -16,9 +16,6 @@ function Base.setindex!(σ::Automorphism, w::Word, x::Letter)
     σ.maps[-x] = -w
 end
 
-Base.getindex(σ::Automorphism{T}, x::T) where {T} = σ[Letter(x)]
-Base.setindex!(σ::Automorphism{T}, w::Word{T}, x::T) where {T} = σ[Letter(x)] = w
-
 Base.show(io::IO, σ::Automorphism) = print(io, "$(typeof(σ))(" * join(("$l => $w" for (l,w) in σ.maps), ", ") * ")")
 Base.show(io::IO, m::MIME"text/plain", σ::Automorphism) = show(io, σ)
 
@@ -52,7 +49,7 @@ end
 Nielsen automorphism mapping `x => x·y`.  
 If `prepend` is `true`, maps `x => y·x`.
 """
-function nielsen(x::Letter{T}, y::Letter{T}, X::Alphabet{T}, prepend::Bool = false) where {T}
+function nielsen(x::Letter, y::Letter, X::Alphabet, prepend::Bool = false)
     σ = Automorphism(X)
     σ[x] = prepend ? Word([y, x]) : Word([x, y])
     return σ
@@ -62,7 +59,7 @@ end
 Whitehead automorphism mapping every `x` in `A` to `x·a`  
 If `x` and `x⁻¹` are in `A`, maps `x` to `a⁻¹·x·a`
 """
-function whitehead(A, a::Letter{T}, X::Alphabet{T}) where {T}
+function whitehead(A, a::Letter, X::Alphabet)
     # slightly restrictive, but then we don't ever have to do wreduce
     @assert !in(a, A) "Element cannot be in set"
     @assert !in(-a, A) "Element cannot be in set"
@@ -72,5 +69,41 @@ function whitehead(A, a::Letter{T}, X::Alphabet{T}) where {T}
         # push!(σ[x], a) does not work, as it will not modify σ[-x]
         σ[x] *= Word([a])
     end
+    return σ
+end
+
+"""
+Automorphism that circularly reduces the given (preferably reduced) word
+"""
+function reduction(w::Word, X::Alphabet)
+    σ = Automorphism(X)
+
+    first = firstindex(w)
+    last = lastindex(w)
+    
+    # as long as the first and last symbol of the word cancel
+    while last > first && -w[first] == w[last]
+        y = w[first]
+
+        # x -> x1·x2·...·xk
+        # turn into -y·x1·x2·...·xk·y if x (or x⁻¹) is in (current sub)word and any xi != y
+        letters = Set(@view w[first:last])
+        for x in X
+            # for generator x, can do this for x or for -x; here, we choose x
+            # maybe it's better to construct letters accordingly and iterate over that? hm.
+            if !x.inv && (x ∈ letters || -x ∈ letters)
+                map = σ[x]
+                if any(a -> a != y && a != -y, map)
+                    # conjugate mapping
+                    # no redundant conjugations will occur if the original word is reduced
+                    σ[x] = Word([-y]) * map * Word([y])
+                end
+            end
+        end
+
+        first += 1
+        last -= 1
+    end
+
     return σ
 end
